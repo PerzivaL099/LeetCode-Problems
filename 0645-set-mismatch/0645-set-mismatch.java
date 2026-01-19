d03cb52bb9c1ed6a1fcdd09d4295d56d81404209
PerzivaL099 <mario152003@gmail.com>
2026-01-19 13:51:10 -0800

    Time: 2 ms (93.94%), Space: 47.3 MB (89.39%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,23 +1,25 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
         int duplicate = -1;
         int missing = -1;
 
-        for (int i=0; i < nums.size(); i++){
+        //Mark Visited with -
+        for (int i = 0; i < n; i++){
             int val = Math.abs(nums[i]);
-
-            if (nums[val - 1] < 0){
+            if(nums[val - 1] < 0){
                 duplicate = val;
             } else {
                 nums[val - 1] *= -1;
             }
         }
-
-        for (int i = 0; i < nums.length; i++){
+        //Index that stays + is the one missing
+        for (int i = 0; i< n; i++){
             if (nums[i] > 0){
                 missing = i + 1;
             }
         }
+        //return index
         return new int[]{duplicate, missing};
     }
 }
